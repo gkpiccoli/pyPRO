bf95309576a253b416723ce88a9915a65cd359a2
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-05-02 20:10:35 -0300

    update Fundamentos/fundamentos.py

--- Fundamentos/fundamentos.py
+++ Fundamentos/fundamentos.py
@@ -1,6 +1,14 @@
+# Tipo int
 num = 1_000_000 #melhor legibilidade 
 print("Teste")
 print(num)
 dir(num) # opções do tipo enumerate
 num.__add__(8)
 print(num)
+
+# Tipo float
+
+
+
+
+# Tipo complex
