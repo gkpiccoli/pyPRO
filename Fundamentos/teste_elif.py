21b4514614ac7038f00ca92460a8516f9ba1530c
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-05-03 15:13:16 -0300

    teste elif

---
+++ Fundamentos/teste_elif.py
@@ -0,0 +1,8 @@
+bebida = input("Que bebida você deseja? :")
+if (bebida == "agua"):
+    print("O Preço da água é R$ 5.00")
+elif(bebida=="coca"):
+    print("A coquinha está R$ 7.00")
+elif(bebida=="guarana"):
+    print("O guaraná está de graça!!!")
+else:print("Desculpe mas não temos a bebida que o Sr. deseja.")
