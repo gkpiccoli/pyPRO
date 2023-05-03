18cbd31f7dc6cc3bd9c1c91fa1f4c2982e7e6c19
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-05-03 10:29:00 -0300

    fundamentos

--- Fundamentos/fundamentos.py
+++ Fundamentos/fundamentos.py
@@ -8,7 +8,26 @@ print(num)
 
 # Tipo float
 
-
-
+a = 14.5
 
 # Tipo complex
+
+x = complex(3,5)
+print(x)
+
+frase = '"Essa frase vai ficar entre aspas?"'
+print(frase)
+
+# ''' quebra de linha interna '''
+
+print(frase[2:16:2])
+print(frase[::-1])
+# [inicio:limite superior: tamanho do passo]
+
+print(frase.split('r'))
+print(frase.split())
+
+x = int(a)
+print(x)
+frase_maisc = frase.upper()
+print(frase_maisc)
