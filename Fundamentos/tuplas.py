b24aba303606eff942674fb912780dd65c426bae
Gustavo Kureck Piccoli <gustavokpiccoli@gmail.com>
2023-05-05 17:01:11 -0300

    tuplas

---
+++ Fundamentos/tuplas.py
@@ -0,0 +1,16 @@
+lista_idades  = []
+while True:
+    idade = int(input("Digite uma idade:"))
+    if idade != -1:
+        lista_idades.append(idade)
+    else:
+        break
+
+tupla_idades = tuple(lista_idades)
+
+quantidade = len(tupla_idades)
+total = sum(tupla_idades)
+media = total / quantidade
+
+print(f"Total de idades é {quantidade:.2f}")
+print(f"Media das idades é {media:.2f}")
